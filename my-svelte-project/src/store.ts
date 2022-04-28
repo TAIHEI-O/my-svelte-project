dd717acd557213df06c0bf9722ec181ef118326d
TAIHEI-O <84520712+TAIHEI-O@users.noreply.github.com>
2022-04-28 10:42:39 +0900

    Update store.ts

--- my-svelte-project/src/store.ts
+++ my-svelte-project/src/store.ts
@@ -1,14 +1,14 @@
 import { writable } from "svelte/store";
 
 type CounterPanel = {
-	id: number,
-	title: string,
-	count: number
+  id: number,
+  title: string,
+  count: number
 }
 export const counterPanel = writable<CounterPanel[]>([
-	{
-		id: Math.floor(Math.random() * 100), 
-		title: "new", 
-		count: 0
-	}
+  {
+    id: Math.floor(Math.random() * 100), 
+    title: "new", 
+    count: 0
+  }
 ])
